9c7984177e0b02398fd63ce022d41e3cbbb8a728
Kirill Golygin <kirik3007@gmail.com>
2023-06-14 14:42:26 +0300

    changed state menegment to reducer in cart and user contexts

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,9 +6,7 @@ import { CartContext } from '../../context/cart.context';
 import './cart-icon.styles.scss'
 
 const CartIcon = () => {
-    const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
-
-    const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
+    const { toggleIsCartOpen, cartCount } = useContext(CartContext);
 
     return (
         <div className='cart-icon-container' onClick={toggleIsCartOpen}>
